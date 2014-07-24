481de2210884372e6132c2dc34cf56614d361899
GAP <glen@screenrev.com>
2014-07-24 14:01:41 +0100

    Added main file and package.json

---
+++ koa-mongo-db.js
@@ -0,0 +1,36 @@
+/*
+	wrap mongodb to return promises
+*/
+
+var config = require('local-config');
+var Promise = require('bluebird');
+var mongodb = require('mongodb');
+var db;
+
+
+module.exports = function(name) {
+	return connect().then(function(db) {
+		return db.collection(name);
+	})
+	.then(function(collection) {
+		Promise.promisifyAll(collection);
+		return collection;
+	});
+};
+
+
+module.exports.ObjectID = mongodb.ObjectID;
+
+
+function connect() {
+	if (db) return Promise.resolve(db);
+	console.log('config.mongodb.url', config.mongodb.url);
+
+	return new Promise(function(resolve, reject) {
+		mongodb.MongoClient.connect(config.mongodb.url, {auto_reconnect: true}, function(err, db) {
+			if (err) return reject(err);
+			Promise.promisifyAll(db);
+			resolve(db);
+		});
+	});
+}
